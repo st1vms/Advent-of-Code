c5ec18237308b856e331333f6445474585ee5bae
st1vms <96783398+st1vms@users.noreply.github.com>
2024-12-04 14:31:06 +0100

    AoC 2024 - Day 4

---
+++ AoC_2024/day_4/day4_p2.py
@@ -0,0 +1,79 @@
+"""AoC 2024 Day 4 Part 2 solution"""
+
+INPUT_FILE = "input.txt"
+
+# Read the input file
+INPUT_MATRIX = []
+with open(INPUT_FILE, "r", encoding="utf-8") as fp:
+    for line in fp.readlines():
+        INPUT_MATRIX.append(list(line.strip()))
+
+
+def get_diagonals_neighbours_cells(
+    matrix: list[list], x_start: int, y_start: int, n_cells: int
+) -> list[list[str]]:
+    """Returns a list of lists of at most `n_cells` neighbours
+    starting at position `x_start` and `y_start` in the `matrix`"""
+
+    directions = [
+        (-1, -1),  # Top-left
+        (-1, 1),  # Top-right
+        (1, -1),  # Bottom-left
+        (1, 1),  # Bottom-right
+    ]
+    neighbors = []
+    rows, cols = len(matrix), len(matrix[0])
+
+    for dx, dy in directions:
+        sub_neighbors = []
+        for step in range(1, n_cells + 1):
+            new_x, new_y = x_start + dx * step, y_start + dy * step
+
+            # Boundary check
+            if 0 <= new_x < rows and 0 <= new_y < cols:
+                sub_neighbors.append(matrix[new_x][new_y])
+            else:
+                # Out of bounds
+                break
+        neighbors.append(sub_neighbors)
+    return neighbors
+
+
+def count_x_word(input_matrix: list[list[str]], three_character_string: str) -> int:
+    """Find total number of times a word is present in the input matrix"""
+    total = 0
+    for x, row in enumerate(input_matrix):
+        for y, ch in enumerate(row):
+
+            # Only count positions that have middle letter
+            if ch != three_character_string[1]:
+                continue
+
+            diagonals_neigh = get_diagonals_neighbours_cells(input_matrix, x, y, 1)
+            if len(diagonals_neigh) < 4:
+                continue
+
+            top_left, top_right, btm_left, btm_right = diagonals_neigh
+
+            # Diagonal check one
+            if three_character_string == "".join(top_left) + ch + "".join(btm_right):
+                if three_character_string == "".join(btm_left) + ch + "".join(
+                    top_right
+                ) or three_character_string == "".join(top_right) + ch + "".join(
+                    btm_left
+                ):
+                    total += 1
+
+            # Diagonal check two
+            if three_character_string == "".join(btm_right) + ch + "".join(top_left):
+                if three_character_string == "".join(btm_left) + ch + "".join(
+                    top_right
+                ) or three_character_string == "".join(top_right) + ch + "".join(
+                    btm_left
+                ):
+                    total += 1
+    return total
+
+
+# Prints solution
+print(f"Number of XMAS: {count_x_word(INPUT_MATRIX, 'MAS')}")
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# AoC 2024 (My attempts)
+# AoC 2024 Writeups
 
-This repository aims to provide the solutions to Advent of Code that I came up with.
+This repository aims to provide the solutions to Advent of Code that I came up with, along with writeups.
 
 The language I will be using to solve the challenges is Python 3.11
 
@@ -9,3 +9,4 @@ The language I will be using to solve the challenges is Python 3.11
 - [Day 1: Historian Hysteria](AoC_2024/day_1/README.md)
 - [Day 2: Red-Nosed Reports](AoC_2024/day_2/README.md)
 - [Day 3: Mull It Over](AoC_2024/day_3/README.md)
+- [Day 4: Ceres Search](AoC_2024/day_4/README.md)
