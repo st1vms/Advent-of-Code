3eb7176b635c2c819d321304520a2cf7e8604136
st1vms <96783398+st1vms@users.noreply.github.com>
2024-12-12 15:13:20 +0100

    AoC 2024 - Day 12 Part 1

---
+++ AoC_2024/day_12/README.md
@@ -0,0 +1,13 @@
+# AoC 2024 [Day 12](https://adventofcode.com/2024/day/12)
+
+## Part 1
+
+Solution file: [Part 1 solution](day12_p1.py)
+
+### Explanation Part 1
+
+## Part 2
+
+Solution file: [Part 2 solution](day12_p2.py)
+
+### Explanation Part 2
---
+++ AoC_2024/day_12/day12_p1.py
@@ -0,0 +1,66 @@
+"""AoC 2024 Day 12 Part 1 solution"""
+
+from itertools import product
+
+INPUT_FILE = "input.txt"
+
+
+def read_input(file_path: str) -> list[str]:
+    """Reads the input map from the given file."""
+    with open(file_path, "r", encoding="utf-8") as file:
+        return [line.strip() for line in file.readlines()]
+
+
+def calculate_area_and_perimeter(
+    grid: list[str], x: int, y: int, visited: list[tuple[int]]
+) -> tuple[int]:
+    """Calculates the area and perimeter of a region using BFS."""
+    plant_type = grid[x][y]
+    queue = [(x, y)]
+    visited.add((x, y))
+
+    area = 0
+    perimeter = 0
+
+    directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+    while queue:
+        cx, cy = queue.pop(0)
+        area += 1
+
+        for dx, dy in directions:
+            nx, ny = cx + dx, cy + dy
+
+            if 0 <= nx < len(grid) and 0 <= ny < len(grid[0]):
+                if grid[nx][ny] == plant_type and (nx, ny) not in visited:
+                    visited.add((nx, ny))
+                    queue.append((nx, ny))
+                elif grid[nx][ny] != plant_type:
+                    perimeter += 1
+            else:
+                perimeter += 1
+
+    return area, perimeter
+
+
+def calculate_total_price(grid: list[str]) -> int:
+    """Calculates the total price of fencing all regions on the map"""
+    visited = set()
+    total_price = 0
+
+    for x, y in product(range(len(grid)), range(len(grid[0]))):
+        if (x, y) not in visited:
+            area, sides = calculate_area_and_perimeter(grid, x, y, visited)
+            total_price += area * sides
+
+    return total_price
+
+
+def _main() -> None:
+    grid = read_input(INPUT_FILE)
+    total_price = calculate_total_price(grid)
+    print(f"The total price of fencing all regions is: {total_price}")
+
+
+if __name__ == "__main__":
+    _main()
